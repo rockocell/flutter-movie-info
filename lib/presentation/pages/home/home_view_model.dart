2f1cf80a93220b3eaa5625016425a6ae27d8791b
MJ <gwang907@gmail.com>
2025-05-14 15:55:10 +0900

    fix: fix home view model and test it

--- lib/presentation/pages/home/home_view_model.dart
+++ lib/presentation/pages/home/home_view_model.dart
@@ -2,49 +2,64 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_info_app/domain/entity/movie.dart';
 import 'package:movie_info_app/presentation/providers.dart';
 
-class HomeViewModel extends Notifier<List<Movie>?> {
-  @override
-  List<Movie>? build() {
-    fetchNowPlayingMovies();
-    fetchPopularMovies();
-    fetchTopRatedMovies();
-    fetchUpcomingMovies();
-    return [];
-  }
+// 하나의 뷰모델에서 4가지 상태를 다루기 때문에 상태들을 객체에 담아 관리
+class HomeState {
+  final List<Movie>? nowPlaying;
+  final List<Movie>? popular;
+  final List<Movie>? topRated;
+  final List<Movie>? upcoming;
 
-  Future<void> fetchNowPlayingMovies() async {
-    final fetchNowPlayingMoviesUsecase = ref.read(
-      fetchNowPlayingMoviesUsecaseProvider,
-    );
-    final result = await fetchNowPlayingMoviesUsecase.execute();
-    state = result;
-  }
+  const HomeState({
+    this.nowPlaying,
+    this.popular,
+    this.topRated,
+    this.upcoming,
+  });
 
-  Future<void> fetchPopularMovies() async {
-    final fetchPopularMoviesUsecase = ref.read(
-      fetchPopularMoviesUsecaseProvider,
+  // 상태 변경은 copyWith으로 처리해서 하나를 처리할 때 나머지 3개는 유지되도록
+  HomeState copyWith({
+    List<Movie>? nowPlaying,
+    List<Movie>? popular,
+    List<Movie>? topRated,
+    List<Movie>? upcoming,
+  }) {
+    return HomeState(
+      nowPlaying: nowPlaying ?? this.nowPlaying,
+      popular: popular ?? this.popular,
+      topRated: topRated ?? this.topRated,
+      upcoming: upcoming ?? this.upcoming,
     );
-    final result = await fetchPopularMoviesUsecase.execute();
-    state = result;
-  }
-
-  Future<void> fetchTopRatedMovies() async {
-    final fetchTopRatedMoviesUsecase = ref.read(
-      fetchTopRatedMoviesUsecaseProvider,
-    );
-    final result = await fetchTopRatedMoviesUsecase.execute();
-    state = result;
-  }
-
-  Future<void> fetchUpcomingMovies() async {
-    final fetchUpcomingMoviesUsecase = ref.read(
-      fetchUpcomingMoviesUsecaseProvider,
-    );
-    final result = await fetchUpcomingMoviesUsecase.execute();
-    state = result;
   }
 }
 
-final homeViewModelProvider = NotifierProvider<HomeViewModel, List<Movie>?>(() {
+class HomeViewModel extends Notifier<HomeState> {
+  @override
+  HomeState build() {
+    return const HomeState();
+  }
+
+  Future<void> fetchNowPlayingMovies() async {
+    final result =
+        await ref.read(fetchNowPlayingMoviesUsecaseProvider).execute();
+    state = state.copyWith(nowPlaying: result);
+  }
+
+  Future<void> fetchPopularMovies() async {
+    final result = await ref.read(fetchPopularMoviesUsecaseProvider).execute();
+    state = state.copyWith(popular: result);
+  }
+
+  Future<void> fetchTopRatedMovies() async {
+    final result = await ref.read(fetchTopRatedMoviesUsecaseProvider).execute();
+    state = state.copyWith(topRated: result);
+  }
+
+  Future<void> fetchUpcomingMovies() async {
+    final result = await ref.read(fetchUpcomingMoviesUsecaseProvider).execute();
+    state = state.copyWith(upcoming: result);
+  }
+}
+
+final homeViewModelProvider = NotifierProvider<HomeViewModel, HomeState>(() {
   return HomeViewModel();
 });
